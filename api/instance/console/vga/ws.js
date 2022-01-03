e3f1e78e347c3ca877fa58a3506eb78abab16ca8
Wolfo-Gaming <klokko06@gmail.com>
2022-01-03 19:16:41 +0000

    Co-authored-by: Joseph Maldjian <joseph.maldjian@hye.services>

--- api/instance/console/vga/ws.js
+++ api/instance/console/vga/ws.js
@@ -2,9 +2,9 @@ const { client, ararat, ws: wss, db } = require('../../../../index')
 const { convertID } = require("../../../../lib/converter")
 async function s(ws, req) {
 
-    console.log('new')
+    console.log('Created new VGA Socket')
     var instance = await client.instance(convertID(req.params.instance))
-    var consoles = await instance.console("vga")
+    var consoles = await instance.console("vga", {})
     consoles.operation.onopen = () => {
         ws.on('message', (data) => {
             consoles.operation.send(data, { binary: true })
