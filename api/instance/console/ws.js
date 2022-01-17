82402ccdfba7f82681e5cde207d29164a430b1ee
Wolfo-Gaming <klokko06@gmail.com>
2022-01-17 17:55:24 +0000

    Add authentication to console

--- api/instance/console/ws.js
+++ api/instance/console/ws.js
@@ -1,6 +1,15 @@
 const { client, ararat, ws: wss, db } = require('../../../index')
 const { convertID } = require("../../../lib/converter")
 const colors = require("colors")
+function isJson(str) {
+   try {
+       JSON.parse(str);
+   } catch (e) {
+       return false;
+   }
+   return true;
+}
+
 async function consoles(ws, req) {
    var inst = await client.instance(convertID(req.params.instance))
    if (inst == null) return;
@@ -10,6 +19,7 @@ async function consoles(ws, req) {
       var offlineMessage = false;
       var interval = setInterval(async () => {
          var state = await inst.state()
+         
          if (wss.get(req.params.instance) && state == "Running") {
             clearInterval(interval);
 
@@ -36,7 +46,7 @@ async function consoles(ws, req) {
                   //startExecConsole()
                } else {
 
-                  araratInstance.relationships.magma_cube.images[araratInstance.magma_cube.image_group][araratInstance.magma_cube.image_index].states.running.forEach(sta => {
+                  araratInstance.relationships.magma_cube.states.running.forEach(sta => {
 
 
                      if (data.toString().includes(sta)) {
@@ -65,12 +75,15 @@ async function consoles(ws, req) {
       var offlineMessage = false;
       var interval = setInterval(async () => {
          var state = await inst.state()
+         console.log("awaiting status for console")
          console.log(wss.websockets)
          if (wss.get(req.params.instance) && state == "Running") {
+            console.log("is running for console")
             clearInterval(interval);
 
             ws.send("\r\n")
             var proxy = wss.get(req.params.instance).ws.proxy(ws)
+            console.log("proxied console to user")
             var st = setInterval(async () => {
                if (await inst.state() == "Stopped") {
                   var arr = wss.websockets
@@ -117,7 +130,7 @@ async function consoles(ws, req) {
       }, 1000)
    }
    async function startVGAConsole() {
-      var instance = await client.instance(convertID( req.params.instance))
+      var instance = await client.instance(convertID(req.params.instance))
       var consoles = await instance.console("vga")
       consoles.operation.onmessage = (s) => {
          ws.send(s.data)
@@ -126,22 +139,102 @@ async function consoles(ws, req) {
          consoles.operation.send(s)
       })
       ws.on('close', () => {
-          console.log(consoles.operation.listenerCount("message"))
-          console.log('close')
+         console.log(consoles.operation.listenerCount("message"))
+         console.log('close')
 
       })
 
    }
    console.log(inst.type())
    if (inst.type() == "container" && araratInstance.relationships.magma_cube.stateless == true) {
-      startExecConsole()
+      var authlisten = async (data) => {
+         if (isJson(data.toString())) {
+            try {
+               var s = JSON.parse(data.toString())
+               console.log(s)
+               if (s.event == "authenticate") {
+                  var token = s.data;
+                  var Instance = await ararat.instance(req.params.instance, ["magma_cube"])
+                  if(await Instance.console().verify(token)) {
+                     console.log('start console')
+                     ws.removeEventListener("message", authlisten)
+                     startExecConsole()
+                  } else {
+                     console.log("Not Authed")
+                  }
+               }
+            } catch (error) {
+               console.log(error)
+            }
+         }
+
+      }
+      ws.send(JSON.stringify({event: "ready"}))
+      ws.on('message',authlisten)
+
    } else if (inst.type() == "virtual-machine" && araratInstance.relationships.magma_cube.console == "vga") {
       startVGAConsole()
    } else if (inst.type() == "virtual-machine" && araratInstance.relationships.magma_cube.console == "xterm") {
-      //texat console
+      var authlisten = async (data) => {
+         if (isJson(data.toString())) {
+            try {
+               var s = JSON.parse(data.toString())
+               console.log(s)
+               if (s.event == "authenticate") {
+                  var token = s.data;
+                  var Instance = await ararat.instance(req.params.instance, ["magma_cube"])
+                  if(await Instance.console().verify(token)) {
+                     console.log('start console')
+                     ws.removeEventListener("message", authlisten)
+                     startSerialConsole()
+                  } else {
+                     console.log("Not Authed")
+                  }
+               }
+            } catch (error) {
+               console.log(error)
+            }
+         }
+
+      }
+      ws.send(JSON.stringify({event: "ready"}))
+      ws.on('message',authlisten)
    } else if (inst.type() == "container" && araratInstance.relationships.magma_cube.stateless == false) {
-      console.log('start serial console')
-      startSerialConsole()
+      console.log('start serial123123 console')
+     
+      var authlisten = async (data) => {
+         console.log("SDLFKJD:LKFL")
+         //console.log(data.toString())
+         if (isJson(data.toString())) {
+            try {
+              // console.log(data.toString())
+               var s = JSON.parse(data.toString())
+               //console.log(s)
+               if (s.event == "authenticate") {
+                  var token = s.data;
+                  var Instance = await ararat.instance(req.params.instance, ["magma_cube"])
+                  if(await Instance.console().verify(token)) {
+                     console.log('start console')
+                     ws.removeEventListener("message", authlisten)
+                     startSerialConsole()
+                  } else {
+                     console.log("Not Authed")
+                  }
+               }
+            } catch (error) {
+               console.log(error)
+            }
+         } else {
+            console.log("Is not JSON")
+         }
+
+      }
+      ws.send(JSON.stringify({event: "ready"}))
+      ws.on('message', (data) => {
+         console.log("GOT LE DATA")
+         console.log(data)
+         authlisten(data)
+      })
    }
 
 }
