efd9e8a10bb828301b0a8b0f93c8a203326b4587
Wolfo-Gaming <klokko06@gmail.com>
2022-01-17 17:59:06 +0000

    Add network forward creation

--- api/network/forwards/post.js
+++ api/network/forwards/post.js
@@ -1,11 +1,13 @@
 var { client } = require('../../../index')
+const { convertNetworkID } = require('../../../lib/converter')
 /**
  * 
  * @param {import('express').Request} req 
  * @param {import('express').Response} res 
  */
 module.exports =async function(req,res) {
+    console.log(convertNetworkID(req.body.network))
     if (!req.body.listen_address || !req.body.ports) return res.status(400).send("Malformed request")
-    var network = await client.network(req.bridge)
-    network.createNetworkForward()
+    var network = await client.network(convertNetworkID(req.body.network))
+    network.createNetworkForward(req.body.listen_address, req.body.description)
 }
