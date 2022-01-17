7e49b69515abfb96b98e44f3d2d6d7f6cbb0779d
Wolfo-Gaming <klokko06@gmail.com>
2022-01-17 18:03:06 +0000

    Improve service generation

--- tools/generateservice.js
+++ tools/generateservice.js
@@ -1,29 +1,32 @@
-var pwd = process.env.PWD.split('/')
-pwd.pop()
-if (process.env.USER != 'root') {
-    console.log('Must be run as root')
-    return process.exit(1)
-}
-var service = `
-[Unit]
-Description=Hye lava daemon
-Documentation=https://hye.gg/
-After=network.target
+function generateService(s) {
 
-[Service]
-Type=simple
-User=${process.env.USER}
-ExecStart=/usr/bin/node ${pwd.join('/')}/index.js
-Restart=on-failure
-WorkingDirectory=${pwd.join('/')}
-
-[Install]
-WantedBy=multi-user.target
-`
-const fs =require('fs')
-try {
-    fs.writeFileSync('/etc/systemd/system/lava.service', service)
-} catch (error) {
-   console.log(error) 
+    var service = `
+    [Unit]
+    Description=Hye lava daemon
+    Documentation=https://hye.gg/
+    After=network.target
+    
+    [Service]
+    Type=simple
+    User=root
+    ExecStart=/usr/bin/node /srv/daemon/index.js
+    Restart=on-failure
+    WorkingDirectory=/srv/daemon
+    
+    [Install]
+    WantedBy=multi-user.target
+    `
+    const fs = require('fs')
+    try {
+        fs.writeFileSync('/etc/systemd/system/lava.service', service)
+    } catch (error) {
+       console.log("Error while creating systemd service") 
+    }
+    console.log('Created service\nStart the daemon with "systemctl daemon-reload && systemctl start lava"') 
+    if (process.env.USER != 'root') {
+        console.log('Must be run as root')
+        return process.exit(1)
+    }
+ 
 }
-console.log('Created service\nStart the daemon with "systemctl daemon-reload && systemctl start lava"')
+module.exports = generateService
