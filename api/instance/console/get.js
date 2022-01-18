8325ada8213bfe0b365dc0b57c62999604ff4942
Wolfo-Gaming <klokko06@gmail.com>
2022-01-18 19:37:47 +0000

    Fix incorrect spice websocket URL

--- api/instance/console/get.js
+++ api/instance/console/get.js
@@ -1,4 +1,7 @@
-function spice(req,res) {
-    res.render('spice', {server: req.params.instance})
+const { ararat } = require("../../../index")
+async function spice(req, res) {
+    var node = await (await ararat.instance(req.params.instance, ["node"])).relationships.node;
+    var full = node.address.ssl ? "https" : "http" + node.address.hostname + ":" + node.address.port
+    res.render('spice', { server: req.params.instance, address: full })
 }
 module.exports = spice
