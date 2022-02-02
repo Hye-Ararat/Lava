5fbf7cf47666059e3522b5258fe789a3310ff930
Joseph Maldjian <joseph.maldjian@hye.gg>
2022-02-02 00:00:47 +0000

    Fix file name on backup download

--- api/instance/backups/get.js
+++ api/instance/backups/get.js
@@ -2,6 +2,9 @@ module.exports = async (req, res) => {
     const { client } = require("../../../index");
     const { convertID } = require("../../../lib/converter")
     try {
+        res.setHeader('Content-disposition', 'attachment; filename=' + req.params.instance + ".tar.gz");
+        //this is fetchable, we just need to add a function to the node module
+        //res.setHeader("Content-Length", usage.disk.usage);
         var backup = await (await client.instance(convertID(req.params.instance))).downloadBackup(req.params.backup, res);
     } catch (error) {
         console.log(error);
