edb83440fbbf24304ad3ef856aa0c74a6ffbf58a
Joseph Maldjian <josephavm@icloud.com>
2022-10-01 13:52:35 -0500

    switch to axios

--- routes/v1/instances/[name]/files.js
+++ routes/v1/instances/[name]/files.js
@@ -1,48 +1,33 @@
 import express from "express";
 import http from "http";
+import axios from "axios"
 
 const router = express.Router({ mergeParams: true });
 
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     const { name } = req.params;
     const { path } = req.query;
     const authorization = req.headers.authorization;
-
     const options = {
         socketPath: './lava.sock',
-        path: `/sftp?instance=${name}&path=${encodeURI(path)}`,
     };
+    let fileData;
     try {
-
-
-        const clientRequest = http.request(options, response => {
-            response.setEncoding("utf8");
-            Object.keys(response.headers).forEach(key => {
-                res.setHeader(key, response.headers[key]);
-            });
-            response.on("data", data => {
-                if (data == "File Does Not Exist") {
-                    return res.status(400).send(data);
-                }
-                if (!(data == "null")) {
-                    res.write(data);
-                } else {
-                    return res.send([]);
-                }
-            })
-            response.on("close", () => {
-                res.end();
-            })
-            response.on("error", data => {
-                console.log(data);
-                res.status(500).send(data);
-            });
-        });
-        clientRequest.end();
+        fileData = await axios.get(`/sftp?instance=${name}&path=${encodeURI(path)}`, options)
     } catch (e) {
         console.log(e);
     }
+    Object.keys(fileData.headers).forEach(key => {
+        res.setHeader(key, fileData.headers[key]);
+    });
+    if (fileData.data == "null") {
+        return res.send([]);
+    }
+    if (fileData.data == "File Does Not Exist") {
+        return res.status(400).send(fileData.data);
+    }
+    return res.send(fileData.data);
 });
 
 export default router;
