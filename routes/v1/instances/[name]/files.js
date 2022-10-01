95979931218cbf9a33873a5fca6a49cba93eb04a
Joseph Maldjian <josephavm@icloud.com>
2022-10-01 13:46:57 -0500

    error logging

--- routes/v1/instances/[name]/files.js
+++ routes/v1/instances/[name]/files.js
@@ -13,30 +13,36 @@ router.get("/", (req, res) => {
         socketPath: './lava.sock',
         path: `/sftp?instance=${name}&path=${encodeURI(path)}`,
     };
-    const clientRequest = http.request(options, response => {
-        response.setEncoding("utf8");
-        Object.keys(response.headers).forEach(key => {
-            res.setHeader(key, response.headers[key]);
+    try {
+
+
+        const clientRequest = http.request(options, response => {
+            response.setEncoding("utf8");
+            Object.keys(response.headers).forEach(key => {
+                res.setHeader(key, response.headers[key]);
+            });
+            response.on("data", data => {
+                if (data == "File Does Not Exist") {
+                    return res.status(400).send(data);
+                }
+                if (!(data == "null")) {
+                    res.write(data);
+                } else {
+                    return res.send([]);
+                }
+            })
+            response.on("close", () => {
+                res.end();
+            })
+            response.on("error", data => {
+                console.log(data);
+                res.status(500).send(data);
+            });
         });
-        response.on("data", data => {
-            if (data == "File Does Not Exist") {
-                return res.status(400).send(data);
-            }
-            if (!(data == "null")) {
-                res.write(data);
-            } else {
-                return res.send([]);
-            }
-        })
-        response.on("close", () => {
-            res.end();
-        })
-        response.on("error", data => {
-            console.log(data);
-            res.status(500).send(data);
-        });
-    });
-    clientRequest.end();
+        clientRequest.end();
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 export default router;
