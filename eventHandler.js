37835b29aaf5a239b78f56ffdcb6756899121ef5
Hye <josephavm@icloud.com>
2023-12-30 21:17:15 +0000

    stateless N-VPS support

---
+++ eventHandler.js
@@ -0,0 +1,88 @@
+import { LXDclient as client } from "./incus.js";
+
+export function listen(sockets) {
+    console.log("Event Handler Running")
+    let list = client.events();
+    list.on("message", async (msg) => {
+        if (JSON.parse(msg).metadata.message == "Start finished") {
+            const inst = await client.instance(JSON.parse(msg).metadata.context.instance).data;
+            if (inst.metadata.status == "Running") {
+                let socks;
+                if (inst.metadata.config["image.variant"]) {
+                    if (inst.metadata.config["image.variant"] == "stateless") {
+                        console.log("STATELESS N_VPS")
+                        if (inst.metadata.config["user.stateless-startup"]) {
+                            let user;
+                            let wd;
+                            if (inst.metadata.config["user.stateless-user"]) user = parseInt(inst.metadata.config["user.stateless-user"]);
+                            if (inst.metadata.config["user.stateless-directory"]) wd = inst.metadata.config["user.stateless-directory"];
+                            let environment = {}
+                            Object.keys(inst.metadata.config).forEach((key) => {
+                                if (key.startsWith("environment.")) {
+                                    environment[key.replace("environment.", "")] = inst.metadata.config[key];
+                                }
+                            })
+                            let startup = inst.metadata.config["user.stateless-startup"].split(" ");
+                            startup = startup.map((arg) => {
+                                if (arg.startsWith("$")) {
+                                    return environment[arg.replace("$", "")];
+                                } else {
+                                    return arg;
+                                }
+                            })
+                            console.log(startup, user, wd)
+                            try {
+                                socks = await client.instance(JSON.parse(msg).metadata.context.instance).exec(startup, user, wd)
+                            } catch (error) {
+                                console.log(error);
+                            }
+                            socks["stdout"].addEventListener("message", async (dat) => {
+                                if (dat.data == "") {
+                                    let updInst = await client.instance(JSON.parse(msg).metadata.context.instance).data;
+                                    if (updInst.metadata.status == "Running") {
+                                        await client.instance(JSON.parse(msg).metadata.context.instance).updateState("stop");
+                                        socks["stdin"].close();
+                                    }
+                                }
+                            })
+
+                        }
+                    }
+                }
+                if (!socks) {
+                    try {
+                        socks = await client.instance(JSON.parse(msg).metadata.context.instance).console("console");
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
+                console.log("PUSHING")
+                //log operation secret
+                /**
+                 * @type {WebSocket}
+                 */
+               let out = socks['stdout']
+                console.log(out.url)
+               console.log(socks["stdin"])
+               
+               let secretOut = out.url.split("secret=")[1]
+                let secretIn = socks["stdin"].url.split("secret=")[1]
+                console.log(secretOut)
+                console.log(secretIn)
+                sockets[secretOut] = {
+                    connections: 0,
+                    socket: socks["stdout"]
+                }
+                sockets[secretIn] = {
+                    connections: 0,
+                    socket: socks["stdin"]
+                }
+                /*global.sockets.push({
+                    name: JSON.parse(msg).metadata.context.instance,
+                    socket: socks
+                })*/;
+            }
+        }
+
+    })
+}
